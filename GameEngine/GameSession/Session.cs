f3c0ed4519f8bcdbc84a54db358509311f1b2255
cipher <anf5726@students.uc-bcf.edu.ph>
2024-03-22 21:35:14 +0800

    Add project files.

---
+++ GameEngine/GameSession/Session.cs
@@ -0,0 +1,23 @@
+﻿using GameEngine.GameModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine.GameSession
+{
+    public class Session
+    {
+        public Entity CurrentPlayer { get; set; }
+        public Session()
+        {
+            CurrentPlayer = new Entity();
+            CurrentPlayer.Name = "Bro";
+            CurrentPlayer.CharacterClass = "Swordsman";
+            CurrentPlayer.Gold = 100000;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
+        }
+    }
+}
---
+++ RPGame/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GameEngine.GameSession;
+
+namespace RPGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Session _gameSession;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _gameSession = new Session();
+            DataContext = _gameSession;
+        }
+    }
+}
